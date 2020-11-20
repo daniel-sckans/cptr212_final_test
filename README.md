c2efbc1d567fcd8898959af42fbd082f94e81341
daniel-sckans <evan.daniel@sckans.edu>
2020-11-19 23:55:08 -0600

    update instructions

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 To complete the test, fork this repo and then clone a local copy.  
 
-Leave the "random_array.h" header file unchanged.  It defines an API that you are to both implement and test in the files "random_array.c" and "random_array_test.c", respectively.  
+Leave the "random_array.h" and "random_array_test.c" files unchanged.  They declare and test, respectively, an API that you are to implement in the file "random_array.c".  
 
-Look at the declarations in the header file, and create your own definitions of them in the "random_array.c" file.  Then test your definitions in the "random_array_test.c" file.  
+Look at the declarations in the header file, and create your own definitions of them in the "random_array.c" file.  
 
 The test ends at 11:45AM, and any commits made after that time will not contribute to your grade.  At that point push the changes to your repo and submit a pull request (do not do so beforehand).  
--- random_array_test.c
+++ random_array_test.c
@@ -1,8 +1,23 @@
-// Write your main function here.  
+// Define the functions mentioned below in the "random_array.c" file.  
+// Base those definitions on the "random_array.h" file.  
 
-// It should create a random_array_t* object by calling the random_array_ctor() function.  
-// It should then call the random_array_print() function.  
-// It should then call the random_array_sort() function.  
-// It should then call the random_array_print() function a second time.  
-// It should then print the median value in the array using printf() and random_array_get_median().  
-// Lastly, it should call the random_array_dtor() function.  
+#include <stdio.h>
+#include <stdlib.h>
+#include "random_array.h"
+
+int main(int argc, char** argv) {
+
+    random_array_t* random_array = random_array_ctor(64, 150, 200); 
+
+    random_array_print(random_array); 
+
+    printf("Median: %d\n", random_array_get_median(random_array)); 
+
+    random_array_sort(random_array); 
+
+    random_array_print(random_array); 
+
+    random_array_dtor(random_array); 
+
+    return EXIT_SUCCESS; 
+}
